90b0716c9b99e02e01f67a6d38fb66fbfdffcde0
e2f5db0 <sami.toiskallio@protonmail.com>
2021-09-02 19:54:00 +0300

    Add meta content description

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.85e1ca0972424593aadb36af70560364.js"
+  "/precache-manifest.9153569a835d168e1ee4185a93d05f40.js"
 );
 
 self.addEventListener('message', (event) => {
